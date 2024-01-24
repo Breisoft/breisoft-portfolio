c3d94c32e2426f1b217a5af8d010f7f8d4965a01
Breisoft <company@breisoft.com>
2024-01-24 13:44:35 -0500

    initial commit

---
+++ frontend/src/pages/dashboard/components/index.js
@@ -0,0 +1,1 @@
+export { default as MasonryPortfolio } from './MasonryPortfolio';
---
+++ frontend/src/pages/dashboard/index.js
@@ -0,0 +1,14 @@
+import { MasonryPortfolio } from './components';
+// ==============================|| DASHBOARD - DEFAULT ||============================== //
+
+const DashboardDefault = () => {
+  return (
+    <>
+      <h2>Yo</h2>
+
+      <MasonryPortfolio />
+    </>
+  );
+};
+
+export default DashboardDefault;
